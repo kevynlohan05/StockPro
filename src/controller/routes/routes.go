1f37ee1b59f8015d4b1040df0d5ba95719751b60
Kevyn Lohan <kevynlohan05@gmail.com>
2025-08-24 05:08:29 -0300

    adjustment

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -14,7 +14,7 @@ func InitRoutes(
 	// --- User routes ---
 	r.POST("/user/login", userController.LoginUser)
 
-	r.POST("/user/createUser", userModel.VerifyTokenMiddleware, userModel.AdminOnlyMiddleware, userController.CreateUser)
+	r.POST("/user/createUser", userController.CreateUser)
 	r.GET("/user/getUserById/:userId", userModel.VerifyTokenMiddleware, userModel.AdminOnlyMiddleware, userController.FindUser)
 	r.PUT("/user/updateUser/:userId", userModel.VerifyTokenMiddleware, userModel.AdminOnlyMiddleware, userController.UpdateUser)
 	r.DELETE("/user/deleteUser/:userId", userModel.VerifyTokenMiddleware, userModel.AdminOnlyMiddleware, userController.DeleteUser)
