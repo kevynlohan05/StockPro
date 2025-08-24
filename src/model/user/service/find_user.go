a7f709cd59b91b832a56b7e5769f25b648a39009
kevynlohan05 <kevynlohan05@gmail.com>
2025-08-24 02:39:48 -0300

    Completando service de usuário

--- src/model/user/service/find_user.go
+++ src/model/user/service/find_user.go
@@ -1,10 +1,20 @@
 package service
 
 import (
+	"log"
+
 	"github.com/kevynlohan05/StockPro/src/configuration/rest_err"
 	userModel "github.com/kevynlohan05/StockPro/src/model/user"
 )
 
 func (ud *userDomainService) FindUserByIdServices(id string) (userModel.UserDomainInterface, *rest_err.RestErr) {
-	return nil, nil
+	log.Println("Iniciando FindUserByIdServices service")
+
+	userResult, err := ud.userRepository.FindUserById(id)
+	if err != nil {
+		log.Println("Erro ao buscar usuário no banco de dados: ", err)
+		return nil, err
+	}
+
+	return userResult, nil
 }
