f6e200cdfbe3c11ca2585aed890cbf4085790701
Kevyn Lohan <kevynlohan05@gmail.com>
2025-08-28 23:09:25 -0300

    Adjusting

--- src/controller/model/request/stok_request.go
+++
@@ -1,9 +0,0 @@
-package request
-
-type StockMovementRequest struct {
-	ProductID int    `json:"product_id" binding:"required"`
-	UserID    int    `json:"user_id" binding:"required"`
-	Type      string `json:"type" binding:"required,oneof=entrada saida"`
-	Quantity  int    `json:"quantity" binding:"required,gt=0"`
-	Reason    string `json:"reason"`
-}
