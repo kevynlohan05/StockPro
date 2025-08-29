a72b462471069a23d719ab063c8ec7ae1f1069d7
Kevyn Lohan <kevynlohan05@gmail.com>
2025-08-29 00:04:46 -0300

    implementing cash_balance table after sale

--- backend/src/model/sale/repository/create_sale.go
+++ backend/src/model/sale/repository/create_sale.go
@@ -15,7 +15,6 @@ func (r *saleRepository) CreateSaleTransaction(sale saleModel.SaleDomainInterfac
 	}
 	defer tx.Rollback()
 
-	// 1️⃣ Inserir cabeçalho da venda
 	res, err := tx.Exec(`INSERT INTO sales (user_id, total_value, payment_method) VALUES (?, ?, ?)`,
 		sale.GetUserID(), sale.GetTotal(), sale.GetPaymentMethod())
 	if err != nil {
@@ -26,7 +25,6 @@ func (r *saleRepository) CreateSaleTransaction(sale saleModel.SaleDomainInterfac
 	saleID, _ := res.LastInsertId()
 	sale.SetID(strconv.FormatInt(saleID, 10))
 
-	// 2️⃣ Inserir itens e atualizar estoque
 	for _, item := range sale.GetItems() {
 		// Inserir sales_items
 		_, err := tx.Exec(`INSERT INTO sales_items (sale_id, product_id, quantity, price) VALUES (?, ?, ?, ?)`,
@@ -36,15 +34,14 @@ func (r *saleRepository) CreateSaleTransaction(sale saleModel.SaleDomainInterfac
 			return rest_err.NewInternalServerError("Erro ao inserir item da venda")
 		}
 
-		// Atualizar stock
 		_, err = tx.Exec(`UPDATE stock SET quantity = quantity - ? WHERE product_id = ?`, item.GetQuantity(), item.GetProductID())
 		if err != nil {
 			log.Println("Erro atualizar stock:", err)
 			return rest_err.NewInternalServerError("Erro ao atualizar estoque")
 		}
 
-		// Inserir movement_stock
 		_, err = tx.Exec(`INSERT INTO movements_stock (product_id, user_id, type, quantity, reason) VALUES (?,?, 'saida', ?, ?)`,
+
 			item.GetProductID(), sale.GetUserID(), item.GetQuantity(), "Venda ID "+strconv.FormatInt(saleID, 10))
 		if err != nil {
 			log.Println("Erro movements_stock:", err)
@@ -52,7 +49,6 @@ func (r *saleRepository) CreateSaleTransaction(sale saleModel.SaleDomainInterfac
 		}
 	}
 
-	// 3️⃣ Inserir no caixa
 	_, err = tx.Exec(`INSERT INTO cash (sale_id, user_id, type, value, description) VALUES (?, ?, 'entrada', ?, ?)`,
 		saleID, sale.GetUserID(), sale.GetTotal(), "Venda ID "+strconv.FormatInt(saleID, 10))
 	if err != nil {
@@ -60,7 +56,16 @@ func (r *saleRepository) CreateSaleTransaction(sale saleModel.SaleDomainInterfac
 		return rest_err.NewInternalServerError("Erro ao registrar entrada no caixa")
 	}
 
-	// Commit
+	_, err = tx.Exec(`
+		INSERT INTO cash_balance (payment, value) 
+		VALUES (?, ?) 
+		ON DUPLICATE KEY UPDATE value = value + VALUES(value)
+	`, sale.GetPaymentMethod(), sale.GetTotal())
+	if err != nil {
+		log.Println("Erro atualizar cash_balance:", err)
+		return rest_err.NewInternalServerError("Erro ao atualizar saldo do caixa")
+	}
+
 	if err := tx.Commit(); err != nil {
 		return rest_err.NewInternalServerError("Erro ao salvar transação")
 	}
